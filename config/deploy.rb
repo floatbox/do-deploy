a3fd6330a1c14cfaefe1e8060ce50455b7fb8b6e
Andrey Arefiev <arefiev@gmail.com>
2015-01-12 02:02:34 +0300

    cap rvm -> rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,6 +38,12 @@ set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/ca
 # Default value for keep_releases is 5
 set :keep_releases, 5
 
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.1.4'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+
 namespace :deploy do
 
   desc 'Restart application'
