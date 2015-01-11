96942e8d3650ebc80b84c0e130e9a667fb66be43
Andrey Arefiev <arefiev@gmail.com>
2015-01-11 19:04:32 +0300

    App init commit

---
+++ config/tweetstream.rb
@@ -0,0 +1,9 @@
+require 'tweetstream'
+
+TweetStream.configure do |config|
+  config.consumer_key       = '5vkD3ErfyNuexSdId8WPEGIQb'
+  config.consumer_secret    = 'iaOsZKNnXFJUZx6OxlWdNZ2Dsyosy0WW9O6TxLyM6tCm07RqKV'
+  config.oauth_token        = '16883311-QyM9p04HotcrSEXgsd7XAzOfTylzUkaqDJmBSfXJg'
+  config.oauth_token_secret = 'rQTf9Wryp3ANaczb0Fh9IMxatDWoX9GBxBri4DrQlO026'
+  config.auth_method        = :oauth
+end
